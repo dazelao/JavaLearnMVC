9125b64cff1cd366dda6a9d11fd6f8b4671e343f
theDaze <frisaman@gmail.com>
2023-03-27 09:31:30 +0300

    SpringLearn 1.2 add edit + del

--- src/main/java/crudlearn/dao/PersonDAO.java
+++ src/main/java/crudlearn/dao/PersonDAO.java
@@ -7,30 +7,40 @@ import java.util.ArrayList;
 import java.util.List;
 @Component
 public class PersonDAO {
-
-    private static int COUNT;
+    private static int PEOPLE_COUNT;
     private List<Person> people;
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++COUNT, "Tom"));
-        people.add(new Person(++COUNT, "Som"));
-        people.add(new Person(++COUNT, "Vova"));
-        people.add(new Person(++COUNT, "Ahmed"));
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@gmail.com"));
     }
 
-    public List<Person> index(){
-        return  people;
-    }
-    public Person show(int id){
-        return  people.stream()
-                .filter(person -> person.getId() ==id)
-                .findAny()
-                .orElse(null);
+    public List<Person> index() {
+        return people;
     }
 
-    public void save(Person person){
-        person.setId(++COUNT);
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
         people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
     }
 }
--- src/main/java/crudlearn/models/Person.java
+++ src/main/java/crudlearn/models/Person.java
@@ -1,6 +1,37 @@
 package crudlearn.models;
 
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
 public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+
+    @Min(value = 0, message = "Age should be greater than 0")
+    private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    public Person() {
+
+    }
+
+    public Person(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
     public int getId() {
         return id;
     }
@@ -17,13 +48,19 @@ public class Person {
         this.name = name;
     }
 
-    public Person(int id, String name) {
-        this.id = id;
-        this.name = name;
+    public int getAge() {
+        return age;
     }
 
-    public Person(){}
-    private  int id;
-    private String name;
+    public void setAge(int age) {
+        this.age = age;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
