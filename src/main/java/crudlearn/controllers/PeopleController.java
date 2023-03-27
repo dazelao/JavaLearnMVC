fbe059c90337a5c6adc5497cd8dc15f45653aa79
theDaze <frisaman@gmail.com>
2023-03-27 14:51:24 +0300

    SpringLearn 1.2 add db

--- src/main/java/crudlearn/controllers/PeopleController.java
+++ src/main/java/crudlearn/controllers/PeopleController.java
@@ -57,8 +57,7 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") @Valid Person person,
-                         BindingResult bindingResult,
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
                          @PathVariable("id") int id) {
         if (bindingResult.hasErrors())
             return "people/edit";
